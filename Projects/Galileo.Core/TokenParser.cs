40dd1e56b08dab36781f86b8cfc53794e98d9f66
Kalmar99 <jkr99@live.no>
2023-10-21 13:28:29 +0200

    [minor] add token parser to tokenize documents

---
+++ Projects/Galileo.Core/TokenParser.cs
@@ -0,0 +1,41 @@
+﻿namespace Galileo;
+
+public static class TokenParser
+{
+    public static List<TokenizedDocument> Parse(List<string> documents)
+    {
+        var tokenizedDocuments = new List<TokenizedDocument>();
+        
+        for (var index = 0; index < documents.Count; index++)
+        {
+            var document = documents[index];
+
+            var tokenizedDocument = Parse(document, index);
+            
+            tokenizedDocuments.AddRange(tokenizedDocument);
+        }
+
+        return tokenizedDocuments;
+    }
+    
+    private static IReadOnlyCollection<TokenizedDocument> Parse(string input, int position)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return new List<TokenizedDocument>(0);
+        }
+        
+        var lowerCase = input.ToLower();
+
+        var terms = lowerCase.Split(" ").GroupBy(s => s, (s, enumerable) => new KeyValuePair<string, int>(s, enumerable.Count()));
+
+        return terms.Select(t => MapToTokenizedDocument(t, position)).ToList();
+    }
+
+    private static TokenizedDocument MapToTokenizedDocument(KeyValuePair<string, int> document, int position)
+    {
+        var term = document.Key;
+        var countOfOccurences = document.Value;
+        return new TokenizedDocument(position, term, countOfOccurences);
+    }
+}
---
+++ Projects/Galileo.Core/TokenizedDocument.cs
@@ -0,0 +1,17 @@
+﻿namespace Galileo;
+
+public class TokenizedDocument
+{
+    public TokenizedDocument(int documentPosition, string term, int countOfOccurrences)
+    {
+        DocumentPosition = documentPosition;
+        Term = term;
+        CountOfOccurrences = countOfOccurrences;
+    }
+
+    public int DocumentPosition { get; }
+
+    public string Term { get; }
+
+    public int CountOfOccurrences { get; }
+}

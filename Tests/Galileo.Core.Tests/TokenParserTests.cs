7e144fa1c7c09e724eb8f547dfb3757e1dfe874f
Kalmar99 <jkr99@live.no>
2023-10-21 14:15:24 +0200

    [patch] add tests for token parser

---
+++ Tests/Galileo.Core.Tests/TokenParserTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace Galileo;
+
+[Collection(nameof(Galileo))]
+public class TokenParserTests
+{
+    [Fact]
+    public void Parse_ReturnsTokenizedDocument_WithCorrectListPosition()
+    {
+        // Arrange
+        var documents = new List<string>()
+        {
+            "banana",
+            "apple"
+        };
+
+        // Act
+        var tokenizedDocuments = TokenParser.Parse(documents);
+        var tokenizedAppleDocument = tokenizedDocuments.FirstOrDefault(t => t.Term.Equals("apple"));
+        
+        // Assert
+        Assert.NotNull(tokenizedAppleDocument);
+        Assert.Equal(1, tokenizedAppleDocument.DocumentPosition);
+    }
+    
+    [Fact]
+    public void Parse_ReturnsTokenizedDocument_WithCorrectTermCount()
+    {
+        // Arrange
+        var documents = new List<string>()
+        {
+            "banana",
+            "john is eating an apple , lisa is also eating an apple"
+        };
+
+        // Act
+        var tokenizedDocuments = TokenParser.Parse(documents);
+        var tokenizedAppleDocument = tokenizedDocuments.FirstOrDefault(t => t.Term.Equals("apple"));
+        
+        // Assert
+        Assert.NotNull(tokenizedAppleDocument);
+        Assert.Equal(2, tokenizedAppleDocument.CountOfOccurrences);
+    }
+}

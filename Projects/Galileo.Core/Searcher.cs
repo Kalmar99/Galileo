516213ece6b924d84361f44c9c1885a1dbfe38cd
Kalmar99 <jkr99@live.no>
2023-10-21 13:28:58 +0200

    [minor] add searcher using search index

---
+++ Projects/Galileo.Core/Searcher.cs
@@ -0,0 +1,28 @@
+﻿namespace Galileo;
+
+public class Searcher
+{
+    private readonly SearchIndex _index;
+    private readonly List<string> _documents;
+
+    public Searcher(SearchIndex index, List<string> documents)
+    {
+        _index = index;
+        _documents = documents;
+    }
+
+    public IReadOnlyCollection<string> Search(string searchQuery)
+    {
+        var terms = searchQuery.Split(" ").ToList();
+        var searchResult = new List<string>();
+
+        foreach (var term in terms)
+        {
+            var indexedDocuments = _index.GetDocumentIndexMatchingTerm(term);
+            var documentsMatchingTerm = indexedDocuments.Select(i => _documents[i]);
+            searchResult.AddRange(documentsMatchingTerm);
+        }
+
+        return searchResult.Distinct().ToList();
+    }
+}
